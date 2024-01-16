d5fbca7d130176127c25d8a10fc90b5f9d13db14
EmiliaBartic <48402982+EmiliaBartic@users.noreply.github.com>
2024-01-16 20:31:22 +0200

    cleanup

--- src/main/java/com/project/repositories/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.project.repositories;
-
-import com.project.models.UserEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<UserEntity, Long> {
-    UserEntity findByEmail(String email);
-    UserEntity findByUsername(String username);
-}
